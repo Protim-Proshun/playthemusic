c32dcf7bf04ede30daab5df87e52780d7da7f810
Protim <36602930+Protim-Proshun@users.noreply.github.com>
2018-11-04 23:24:25 +0600

    first

---
+++ PlayTheMusic/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace PlayTheMusic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.txtContents = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(2, 74);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(415, 295);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(958, 64);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(234, 260);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1078, 336);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(114, 33);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Choose Playlist";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(681, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Playlists";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(685, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Happy";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(685, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Sad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(685, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Romantic";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(685, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Instrumental";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(765, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Happy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(765, 341);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Sad";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(765, 387);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Romantic";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(765, 429);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Instrumental";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(958, 64);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(234, 260);
+            this.listBox2.TabIndex = 17;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1078, 336);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(114, 33);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "Choose Playlist";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(958, 64);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(234, 260);
+            this.listBox3.TabIndex = 19;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1078, 336);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(114, 33);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "Choose Playlist";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(1078, 336);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(114, 33);
+            this.button8.TabIndex = 21;
+            this.button8.Text = "Choose Playlist";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 16;
+            this.listBox4.Location = new System.Drawing.Point(958, 66);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(234, 260);
+            this.listBox4.TabIndex = 22;
+            this.listBox4.SelectedIndexChanged += new System.EventHandler(this.listBox4_SelectedIndexChanged);
+            // 
+            // txtContents
+            // 
+            this.txtContents.Location = new System.Drawing.Point(42, 30);
+            this.txtContents.Name = "txtContents";
+            this.txtContents.Size = new System.Drawing.Size(162, 22);
+            this.txtContents.TabIndex = 23;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 477);
+            this.Controls.Add(this.txtContents);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.TextBox txtContents;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PlayTheMusic/Form1.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Threading;
+
+namespace PlayTheMusic
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer ss = new SpeechSynthesizer();
+        PromptBuilder pb = new PromptBuilder();
+        SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
+        SpeechRecognitionEngine sre2 = new SpeechRecognitionEngine();
+        Choices clist = new Choices();
+        Choices clist2 = new Choices();
+        
+        
+        string[] files, paths, files2, paths2, files3, paths3, files4, paths4;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ss.SpeakAsync("Welcome to play the music. Let me know your mood           ");
+            timer1.Start();
+            clist.Add(new string[] { "Happy", "Sad", "Romantic","Instrumental","No"});
+            clist2.Add(new string[] { "Yes", "No" });
+            Grammar gr2 = new Grammar(new GrammarBuilder(clist2));
+            Grammar gr = new Grammar(new GrammarBuilder(clist));
+            
+
+            try
+            {
+                sre.RequestRecognizerUpdate();
+                sre.LoadGrammar(gr);
+                sre.SpeechRecognized += sre_SpeechRecognized;
+                sre.SetInputToDefaultAudioDevice();
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+
+                sre2.RequestRecognizerUpdate();
+                sre2.LoadGrammar(gr2);
+                sre2.SpeechRecognized += sre2_SpeechRecognized;
+                sre2.SetInputToDefaultAudioDevice();
+                sre2.RecognizeAsync(RecognizeMode.Multiple);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        void sre2_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text.ToString())
+            {
+                case "Yes":
+                    sre.Recognize();
+                    break;
+
+                case "No":
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+                    sre.RecognizeAsyncStop();
+                    break;
+
+            }
+        }
+
+        
+        void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text.ToString())
+            {
+                case "Happy":
+                    button5.Visible = true;
+                    listBox1.Visible = true;
+
+                    button6.Visible = false;
+                    listBox2.Visible = false;
+
+                    button7.Visible = false;
+                    listBox3.Visible = false;
+
+                    button8.Visible = false;
+                    listBox4.Visible = false;
+
+                    axWindowsMediaPlayer1.Ctlcontrols.stop();
+                    sre.RecognizeAsyncStop();
+                    break;
+
+                case "Sad":
+                    button5.Visible = false;
+                    listBox1.Visible = false;
+
+                    button6.Visible = true;
+                    listBox2.Visible = true;
+
+                    button7.Visible = false;
+                    listBox3.Visible = false;
+
+                    button8.Visible = false;
+                    listBox4.Visible = false;
+
+                    axWindowsMediaPlayer1.Ctlcontrols.stop();
+                    sre.RecognizeAsyncStop();
+                    break;
+
+                case "Romantic":
+                    button5.Visible = false;
+                    listBox1.Visible = false;
+
+                    button6.Visible = false;
+                    listBox2.Visible = false;
+
+                    button7.Visible = true;
+                    listBox3.Visible = true;
+
+                    button8.Visible = false;
+                    listBox4.Visible = false;
+
+                    axWindowsMediaPlayer1.Ctlcontrols.stop();
+                    sre.RecognizeAsyncStop();
+                    break;
+
+                case "Instrumental":
+                    button5.Visible = false;
+                    listBox1.Visible = false;
+
+                    button6.Visible = false;
+                    listBox2.Visible = false;
+
+                    button7.Visible = false;
+                    listBox3.Visible = false;
+
+                    button8.Visible = true;
+                    listBox4.Visible = true;
+
+                    axWindowsMediaPlayer1.Ctlcontrols.stop();
+                    sre.RecognizeAsyncStop();
+                    break;
+
+                case "No":
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+                    break;
+
+
+                /*case "open chrome":
+                Process.Start("chrome", "http://www.youtube.com");
+                break;
+                case "close":
+                    Application.Exit();
+                    break;*/
+
+
+            }
+            txtContents.Text += e.Result.Text.ToString() + Environment.NewLine;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Enable Happy mode
+            button5.Visible = true;
+            listBox1.Visible = true;
+            
+            button6.Visible = false;
+            listBox2.Visible = false;
+            
+            button7.Visible = false;
+            listBox3.Visible = false;
+            
+            button8.Visible = false;
+            listBox4.Visible = false;
+            
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //happy mode song selection
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Multiselect = true;
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                files = openFileDialog1.SafeFileNames;  //save only the names
+                paths = openFileDialog1.FileNames;      //save the full paths
+                for (int i = 0; i < files.Length; i++)
+                {
+                    listBox1.Items.Add(files[i]);     //Add songs to the listbox
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //happy mode
+            axWindowsMediaPlayer1.URL=paths[listBox1.SelectedIndex];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Enable Sad Mode
+            button5.Visible = false;
+            listBox1.Visible = false;
+
+            button6.Visible = true;
+            listBox2.Visible = true;
+
+            button7.Visible = false;
+            listBox3.Visible = false;
+
+            button8.Visible = false;
+            listBox4.Visible = false;
+
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+          
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //sad mode song selection
+
+            OpenFileDialog openFileDialog2 = new OpenFileDialog();
+            openFileDialog2.Multiselect = true;
+
+            if (openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                files2 = openFileDialog2.SafeFileNames;  //save only the names
+                paths2 = openFileDialog2.FileNames;      //save the full paths
+                for (int i = 0; i < files2.Length; i++)
+                {
+                    listBox2.Items.Add(files2[i]);     //Add songs to the listbox
+                }
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //sad mode
+            axWindowsMediaPlayer1.URL = paths2[listBox2.SelectedIndex];
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Romantic Mode
+            button5.Visible = false;
+            listBox1.Visible = false;
+
+            button6.Visible = false;
+            listBox2.Visible = false;
+
+            button7.Visible = true;
+            listBox3.Visible = true;
+
+            button8.Visible = false;
+            listBox4.Visible = false;
+
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //Romantic mode song selection
+            OpenFileDialog openFileDialog3 = new OpenFileDialog();
+            openFileDialog3.Multiselect = true;
+
+            if (openFileDialog3.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                files3 = openFileDialog3.SafeFileNames;  //save only the names
+                paths3 = openFileDialog3.FileNames;      //save the full paths
+                for (int i = 0; i < files3.Length; i++)
+                {
+                    listBox3.Items.Add(files3[i]);     //Add songs to the listbox
+                }
+            }
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Romantic mode
+            axWindowsMediaPlayer1.URL = paths3[listBox3.SelectedIndex];
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            //Instrumenatal
+            button5.Visible = false;
+            listBox1.Visible = false;
+
+            button6.Visible = false;
+            listBox2.Visible = false;
+
+            button7.Visible = false;
+            listBox3.Visible = false;
+
+            button8.Visible = true;
+            listBox4.Visible = true;
+
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //Instrumental mode song selection
+            OpenFileDialog openFileDialog4 = new OpenFileDialog();
+            openFileDialog4.Multiselect = true;
+
+            if (openFileDialog4.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                files4 = openFileDialog4.SafeFileNames;  //save only the names
+                paths4 = openFileDialog4.FileNames;      //save the full paths
+                for (int i = 0; i < files4.Length; i++)
+                {
+                    listBox4.Items.Add(files4[i]);     //Add songs to the listbox
+                }
+            }
+        }
+
+        private void listBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Instrumental mode
+            axWindowsMediaPlayer1.URL = paths4[listBox4.SelectedIndex];
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ss.SpeakAsync("Want to change the playlist");
+            sre.Recognize();
+
+        }
+
+        
+            
+        }
+        
+    }
